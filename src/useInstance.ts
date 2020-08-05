26fddd53d96ffc15005d69b0a4e92cf0f8956e63
HuiAzir <huiazr@qq.com>
2020-08-05 09:08:24 +0800

    feat: useInstance

---
+++ src/useInstance.ts
@@ -0,0 +1,16 @@
+import { useRef } from 'react'
+import { isFunction } from './utils'
+
+function useInstance<T> (initial?: T | (()=>T)): T | undefined {
+  const instance = useRef<T>()
+  if (instance.current === undefined) {
+    if (initial) {
+      instance.current = isFunction(initial) ? initial() : initial
+    } else {
+      instance.current = {} as T
+    }
+  }
+  return instance.current
+}
+
+export default useInstance
