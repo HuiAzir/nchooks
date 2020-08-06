324eb287bea0a46f2571842b461b581ddc4eac66
tenghui.cwh <tenghui.cwh@cainiao.com>
2020-08-06 14:21:38 +0800

    chore: add jest ignore dir

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   clearMocks: true,
+  modulePathIgnorePatterns: ['<rootDir>/esm/', '<rootDir>/lib/', '<rootDir>/node_modules/'],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.json'
