fba6e5dd7196d844472cee78f0af6b968439ece5
tenghui.cwh <tenghui.cwh@cainiao.com>
2020-08-07 12:31:23 +0800

    fix: fix useImmutable bug

--- src/useImmutable/index.ts
+++ src/useImmutable/index.ts
@@ -2,7 +2,8 @@ import { useCallback, useState, Dispatch } from 'react'
 import produce from 'immer'
 
 function useImmutable<T> (initialValue?: T | (() => T)): [ T | undefined, Dispatch<any>] {
-  const [state, setState] = useState<T | undefined>(initialValue)
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const [state, setState] = useState<T | undefined>(produce(initialValue, () => { }))
   return [
     state,
     useCallback<Dispatch<any>>((updater:any) => {
