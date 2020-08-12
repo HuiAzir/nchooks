caf32881d104fc88536f4bb3b5001e82c522be36
HuiAzir <huiazr@qq.com>
2020-08-12 23:20:09 +0800

    feat: add get uuid

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,16 @@
 export function isFunction<S> (initial?: S | (() => S)): initial is () => S {
   return typeof initial === 'function'
 }
+
+export function generateUUID (): string {
+  let d = new Date().getTime()
+  if (window.performance && typeof window.performance.now === 'function') {
+    d += performance.now() // use high-precision timer if available
+  }
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (d + Math.random() * 16) % 16 | 0
+    d = Math.floor(d / 16)
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+  })
+  return uuid
+}
