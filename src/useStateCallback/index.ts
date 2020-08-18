a5f429806523e255ce289c63485937de705f091d
楚岚 <chulan.lyx@cainiao.com>
2020-08-18 14:57:04 +0800

    upodate

---
+++ src/useStateCallback/index.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState, useRef } from 'react'
+
+const useStateCallback = (initial: any): any => {
+    let isUpdate = useRef<any>();
+    const [state, setState] = useState(initial)
+    const enhanceState = (state, func) => {
+        setState(val => {
+            isUpdate.current = func;
+            return typeof state === 'function' ? state(val) : state
+        })
+    }
+    useEffect(() => {
+        isUpdate.current?.(state);
+    })
+    return [state, enhanceState]
+}
+
+export default useStateCallback;
